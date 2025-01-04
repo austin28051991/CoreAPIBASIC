5a1b39d4e013ab06a10d634a0b9f9cdd04c2809f
Vaibhav <vaibhavshuklalko@gmail.com>
2025-01-04 22:34:49 +0530

    First Commit

---
+++ CoreWebAPI/CoreWebAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,84 @@
+﻿using CoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace CoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+        public StudentAPIController(StudentDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudent()
+        {
+            var data=await _context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(long id)
+        {
+            var student=await _context.Students.FindAsync(id);
+            if(student==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(student);
+            }
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await _context.Students.AddAsync(std);
+            await _context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id,Student std)
+        {
+            if(id!=std.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.Entry(std).State= EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(std);
+            }
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(long id)
+        {
+            var std = await _context.Students.FindAsync(id);
+            if(std==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Students.Remove(std);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+           
+        }
+
+    }
+}
---
+++ CoreWebAPI/CoreWebAPI/Models/StudentDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebAPI.Models;
+
+public partial class StudentDbContext : DbContext
+{
+    public StudentDbContext()
+    {
+    }
+
+    public StudentDbContext(DbContextOptions<StudentDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.FatherName).HasMaxLength(100);
+            entity.Property(e => e.Standard).HasMaxLength(50);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(50)
+                .HasColumnName("Student_Gender");
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .HasColumnName("Student_Name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
